a07ba33ff7d380153b6b111f97090a77e66c713f
unknown <aarmijos062_est@instipp.edu.ec>
2025-06-10 20:05:39 -0500

    Inicio de Proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Socio } from './socio/components/socio/socio';
+import { ReporteMensualidades } from './socio/components/reporte-mensualidades/reporte-mensualidades';
+
+export const routes: Routes = [
+  {path: '', component: Socio},
+  {path: 'socio', component: Socio},
+  {path: 'reportemensualidades', component: ReporteMensualidades},
+  {path: '**', redirectTo: ''}
+];
+
+//indica las rutas de navegación
